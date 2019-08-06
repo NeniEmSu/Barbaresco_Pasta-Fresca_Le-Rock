8e56246626f31efa1466a81f0cdf820f0d78cbff
Emmanuel Neni <emmanuelneni@gmail.com>
2019-08-06 18:48:31 +0300

    done with design and layout of home page

---
+++ components/homePageNavigationCard.vue
@@ -0,0 +1,199 @@
+<template>
+  <div>
+    <b-card
+      :img-src="require(`~/assets/img/${imageshowcase + '.png'}`)"
+      :img-alt="showcasealt"
+      img-top
+      tag="article"
+      style="max-width: 350px;"
+      class="mb-2 homeNavCard"
+    >
+      <div class="cardContent">
+        <div class="card-log mx-auto text-center">
+          <b-img-lazy
+            class="mx-auto"
+            :src="require(`~/assets/img/${logo + '.svg'}`)"
+            :alt="logoalt"
+          ></b-img-lazy>
+        </div>
+        <b-card-text class="cardDescription">
+          {{ details }}
+        </b-card-text>
+        <div class="text-center goTosite">
+          <b-button class="mx-auto goToSiteBtn" :to="sitelink"
+            >Перейти до закладу</b-button
+          >
+        </div>
+        <div class="text-center ">
+          <b-button class="socialButtons instagram mx-auto" :href="instalink">
+            <b-img-lazy
+              class="inButtonImg "
+              src="~assets/img/instagram.svg"
+              alt="instagram text"
+            ></b-img-lazy>
+          </b-button>
+          <b-button
+            class="socialButtons facebook mx-auto"
+            target="_blank"
+            :href="faceblink"
+          >
+            <b-img-lazy
+              class="inButtonImg "
+              src="~assets/img/facebook.svg"
+              alt="facebook text"
+            ></b-img-lazy>
+          </b-button>
+        </div>
+      </div>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    imageshowcase: {
+      type: String,
+      required: true
+    },
+    showcasealt: {
+      type: String,
+      required: true
+    },
+    logo: {
+      type: String,
+      required: true
+    },
+    logoalt: {
+      type: String,
+      required: true
+    },
+    details: {
+      type: String,
+      required: true
+    },
+    sitelink: {
+      type: String,
+      required: true
+    },
+    instalink: {
+      type: String,
+      required: true
+    },
+    faceblink: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.homeNavCard {
+  background: #0a0a0a;
+  box-shadow: 0px 15px 45px rgba(0, 0, 0, 0.55);
+  border: 0;
+}
+
+.cardContent {
+  padding: 35px 15px;
+}
+
+.card-log {
+  height: 80px;
+  width: 160px;
+  margin-bottom: 35px;
+}
+
+.cardDescription {
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 18px;
+  line-height: 21px;
+  text-align: center;
+
+  color: #ffffff;
+}
+
+.goTosite {
+  margin-bottom: 10px;
+}
+
+.goToSiteBtn {
+  border: 2px solid #ffffff;
+  border-radius: 0;
+  box-sizing: border-box;
+
+  background-color: transparent;
+
+  padding: 14px 55px;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 21px;
+
+  color: #ffffff;
+
+  &:hover {
+    background: #ffffff;
+    color: #000000;
+  }
+}
+
+.socialButtons {
+  border: 2px solid #ffffff;
+  border-radius: 0;
+  box-sizing: border-box;
+
+  background-color: transparent;
+
+  padding: 14px 25px;
+
+  &.instagram {
+    margin: 10px auto;
+  }
+
+  &.instagram:hover {
+    background: -moz-linear-gradient(
+      69.74deg,
+      #fdd276 -7.77%,
+      #e1585f 55.36%,
+      #a741b6 106.84%
+    );
+    background: -webkit-linear-gradient(
+      69.74deg,
+      #fdd276 -7.77%,
+      #e1585f 55.36%,
+      #a741b6 106.84%
+    );
+    background: linear-gradient(
+      69.74deg,
+      #fdd276 -7.77%,
+      #e1585f 55.36%,
+      #a741b6 106.84%
+    );
+
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#f09433', endColorstr='#bc1888',GradientType=1 );
+  }
+
+  &.facebook {
+    margin: 10px auto;
+  }
+
+  &.facebook:hover {
+    background-color: #395795;
+  }
+}
+
+.inButtonImg {
+  width: auto;
+  height: 20px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,126 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        barbaresco
-      </h1>
-      <h2 class="subtitle">
-        All top restaurants in ternopil in one place
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div id="homePage">
+    <div class="mx-auto">
+      <h1 class="homePageHeading">Оберіть заклад</h1>
+
+      <div class="col-10 offset-1">
+        :
+        <div class="row">
+          <homePageNavigationCard
+            v-for="variant in variants"
+            :key="variant.name"
+            class="col-3 mx-auto"
+            :name="variant.name"
+            :imageshowcase="variant.imageShowcase"
+            :showcasealt="variant.showcaseAlt"
+            :logo="variant.logo"
+            :logoalt="variant.logoAlt"
+            :sitelink="variant.siteLink"
+            :details="variant.details"
+            :instalink="variant.instaLink"
+            :faceblink="variant.facebLink"
+          ></homePageNavigationCard>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import homePageNavigationCard from '~/components/homePageNavigationCard'
 
 export default {
   components: {
-    Logo
+    homePageNavigationCard
+  },
+  data() {
+    return {
+      variants: [
+        {
+          name: 'Barbaresco',
+          imageShowcase: 'home-page-barbaresco',
+          showcaseAlt: 'Image of barbaresco',
+          logo: 'logo-barbaresco',
+          logoAlt: 'barbaresco logo',
+          siteLink: '#',
+          details:
+            'Затишна атмосфера, приємна музика, смачна кухня та привітний персонал - все це Ви знайдете у Нас',
+          instaLink: '#',
+          facebLink: 'https://www.facebook.com/barbarescocitycafe/'
+        },
+        {
+          name: 'Pasta Fresca',
+          imageShowcase: 'home-page-pasta-fresca',
+          showcaseAlt: 'Image of Pasta Fresca',
+          logo: 'logo-pasta',
+          logoAlt: 'pasta-fresca-logo',
+          siteLink: '#',
+          details:
+            'Відкриваємо суперздібності від смаку нашої пасти Приходьте та спробуйте самі',
+          instaLink: '#',
+          facebLink: 'https://www.facebook.com/barbarescocitycafe/'
+        },
+        {
+          name: 'Le rock',
+          imageShowcase: 'home-page-le-rock',
+          showcaseAlt: 'Image of Le rock',
+          logo: 'logo-le-rock',
+          logoAlt: 'Le rock logo',
+          siteLink: '#',
+          details:
+            'Компанія друзів, смачне меню і відмінна атмосфера - тільки в Le Rock!',
+          instaLink: '#',
+          facebLink: 'https://www.facebook.com/barbarescocitycafe/'
+        },
+        {
+          name: 'Piazza white',
+          imageShowcase: 'home-page-piazza-white',
+          showcaseAlt: 'Image of Piazza white',
+          logo: 'logo-piazza-white',
+          logoAlt: 'Piazza white logo',
+          siteLink: '#',
+          details:
+            'Затишна атмосфера, приємна музика, смачна кухня та привітний персонал - все це Ви знайдете у Нас',
+          instaLink: '#',
+          facebLink: 'https://www.facebook.com/barbarescocitycafe/'
+        }
+      ]
+    }
   }
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+<style lang="scss" scoped>
+#homePage {
+  position: absolute;
+  z-index: -3;
+  background-image: url('~assets/img/home-page-bg.png');
+  background-color: #000000;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+
+  opacity: 0.9;
+  // filter: blur(5px);
+
+  height: 100vh;
+  width: 100vw;
+}
+.homePageHeading {
   text-align: center;
-}
+  margin-top: 70px;
+  margin-bottom: 70px;
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+  font-family: Segoe UI;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 40px;
+  line-height: 53px;
+  letter-spacing: 0.2em;
+  text-transform: uppercase;
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+  color: #ffffff;
 
-.links {
-  padding-top: 15px;
+  opacity: 0.8;
 }
 </style>
